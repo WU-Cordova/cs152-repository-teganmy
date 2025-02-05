aae56eaff818dabae6ea295d570efee343c3e879
teganmy <trmyers@willamette.edu>
2025-02-05 12:30:23 -0800

    new files

--- projects/project1/program.py
+++ projects/project1/program.py
@@ -1,12 +1,14 @@
 
 from datastructures.bag import Bag
+from projects.project1.card import Card, CardSuit, CardFace
 
 def main():
-    bag = Bag()
-    
-    print("Hello, World!")
+    card_suits = [suit.value for suit in list (CardSuit)]
+    print(card_suits)
 
-
+    for suit in list(CardSuit):
+        for face in list(CardFace):
+            card_suits.append(Card(suit.value, face.value))
 
 if __name__ == '__main__':
     main()
